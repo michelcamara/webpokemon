040791d69aec5621af639d7dec24668437ccf1b5
Michel Camara <michelvictor16@gmail.com>
2020-03-18 17:39:26 -0400

    Filtro
    Funcao que filtra os pokemons

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 import './App.css';
-
 import pokebola from './assets/pokebola.png';
+import PokeForm from './components/PokeForm';
+
 
 function App() {
   const [poke, setPoke] = useState([]);
   const [tipos, setTipos] = useState([]);
+  
 
   useEffect(() => {
     async function loadPokemons() {
@@ -30,29 +32,34 @@ function App() {
     loadPokemons();
   }, []);
 
-  function filterPoks(tipo) {
-    console.log(tipo);
+
+
+  //Funcao que é disparada quando o botao de submit for clicado
+ async function handleTypes(data) {
+    const {tipo}= data;
     const section = [];
-    poke.map(pok => (
-      pok.map(p => {
-        if (p.type[0] === tipo || p.type[1] === tipo) {
-          section.push(pok);
-        }
-      })
-    ))
+    const response = await axios.get('https://raw.githubusercontent.com/Biuni/PokemonGO-Pokedex/master/pokedex.json');
+    // eslint-disable-next-line array-callback-return
+    response.data.pokemon.map(pok => {
+      if(pok.type[0] === tipo || pok.type[1] === tipo){
+        section.push(pok);
+      }
+    })
+   
     setPoke(section);
+    
   }
 
   return (
     <div id="app">
       <div className="asidediv">
         <aside>
-          <form onSubmit={filterPoks}>
+          <form>
             <div className="input-block">
               <img src={pokebola} alt="Pokebola" />
               <label>Filtrar Pokémon</label>
               {tipos.map(tipo => (
-                <button key={tipo} className={tipo} value={tipo} type="submit">{tipo}</button>
+                <PokeForm key={tipo} onSubmit={handleTypes} tipo={tipo} />
               ))}
             </div>
           </form>
---
+++ src/components/PokeForm.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+
+export default function PokeForm({ tipo, onSubmit, }) {
+
+  async function handleSubmit(e) {
+    //prevendo o comportamento padrao do formulario
+    e.preventDefault();
+    await onSubmit({
+      tipo,
+    });
+  }
+
+  return (
+    <button key={tipo} className={tipo} type="submit" onClick={handleSubmit}>{tipo}</button>
+  )
+} 
